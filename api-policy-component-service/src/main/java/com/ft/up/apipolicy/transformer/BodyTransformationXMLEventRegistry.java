8e40593b96ae8e7d1b62b8c80f61a3ab546f4746
Hunor Kovács <hunor.kovacs@ft.com>
2015-02-11 17:41:37 +0200

    Added filter for ft-content tag filtering in xml body, where its property type is of ImageSet.

--- api-policy-component-service/src/main/java/com/ft/up/apipolicy/transformer/BodyTransformationXMLEventRegistry.java
+++ api-policy-component-service/src/main/java/com/ft/up/apipolicy/transformer/BodyTransformationXMLEventRegistry.java
@@ -1,12 +1,11 @@
 package com.ft.up.apipolicy.transformer;
 
-import com.ft.bodyprocessing.xml.eventhandlers.PlainTextHtmlEntityReferenceEventHandler;
-import com.ft.bodyprocessing.xml.eventhandlers.RetainXMLEventHandler;
-import com.ft.bodyprocessing.xml.eventhandlers.StripElementAndContentsXMLEventHandler;
-import com.ft.bodyprocessing.xml.eventhandlers.StripElementByClassEventHandler;
-import com.ft.bodyprocessing.xml.eventhandlers.XMLEventHandlerRegistry;
+import com.ft.bodyprocessing.xml.eventhandlers.*;
 
 public class BodyTransformationXMLEventRegistry extends XMLEventHandlerRegistry {
+
+    private static final String IMAGE_SET_ONTOLOGY = "http://www.ft.com/ontology/content/ImageSet";
+    private static final String FT_CONTENT = "ft-content";
 
     public BodyTransformationXMLEventRegistry() {
 
@@ -19,6 +18,7 @@ public class BodyTransformationXMLEventRegistry extends XMLEventHandlerRegistry 
         registerStartAndEndElementEventHandler(new StripElementAndContentsXMLEventHandler(), "pull-quote");
 		registerStartAndEndElementEventHandler(new StripElementByClassEventHandler("twitter-tweet", new RetainXMLEventHandler()), "blockquote");
 		registerStartAndEndElementEventHandler(new StripElementAndContentsXMLEventHandler(), "timeline", "table", "big-number");
-
+		registerStartAndEndElementEventHandler(new StripElementByTypeEventHandler(IMAGE_SET_ONTOLOGY,
+                new RetainXMLEventHandler()), FT_CONTENT);
     }
 }
