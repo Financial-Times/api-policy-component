b8e43dbf68a4288293d580d19ea8b0870309e961
Veselin Vlasakiev <veselin.vlasakiev@ft.com>
2023-12-06 12:53:13 +0200

    publication read policy checker
    Signed-off-by: Veselin Vlasakiev <veselin.vlasakiev@ft.com>

---
+++ api-policy-component-service/src/main/java/com/ft/up/apipolicy/filters/CheckPublicationPolicy.java
@@ -0,0 +1,98 @@
+package com.ft.up.apipolicy.filters;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ft.up.apipolicy.JsonConverter;
+import com.ft.up.apipolicy.pipeline.ApiFilter;
+import com.ft.up.apipolicy.pipeline.HttpPipelineChain;
+import com.ft.up.apipolicy.pipeline.MutableRequest;
+import com.ft.up.apipolicy.pipeline.MutableResponse;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+public class CheckPublicationPolicy implements ApiFilter {
+  private static final String POLICY = "X-Policy";
+  private static final String PUBLICATION = "publication";
+  private static final String PUBLICATION_PREFIX = "PBLC_READ_";
+  private static final String PINK_FT = "88fdde6c-2aa4-4f78-af02-9f680097cfd6";
+  private final JsonConverter jsonConverter;
+
+  public CheckPublicationPolicy(JsonConverter jsonConverter) {
+    this.jsonConverter = jsonConverter;
+  }
+
+  @Override
+  public MutableResponse processRequest(MutableRequest request, HttpPipelineChain chain) {
+    final MutableResponse response = chain.callNextFilter(request);
+    if (isEligibleForPublicationPolicyCheck(response)) {
+      final Map<String, Object> content = extractContent(response);
+      MultivaluedMap<String, Object> httpHeaders = response.getHeaders();
+      Set<String> policies = getPublicationPolicies(httpHeaders.get(POLICY));
+      List<String> publication = convertObjectToStringList(content.get(PUBLICATION));
+      if (!checkAccess(policies, publication)) {
+        response.setStatus(403);
+      }
+    }
+    return response;
+  }
+
+  private static Boolean checkAccess(Set<String> policies, List<String> publication) {
+    for (String p : policies) {
+      if (publication.contains(p)) {
+        return true;
+      }
+    }
+    // No publication related X-Policy or publication field we consider this legacy ft request
+    return policies.isEmpty() && publication.contains(PINK_FT) || publication.isEmpty();
+  }
+
+  private static List<String> convertObjectToStringList(Object obj) {
+    if (obj instanceof Collection) {
+      return new ObjectMapper().convertValue(obj, new TypeReference<List<String>>() {});
+    } else {
+      return Collections.emptyList();
+    }
+  }
+
+  private Set<String> getPublicationPolicies(List<Object> headerPolicies) {
+    Set<String> policies = new LinkedHashSet<>();
+    if (headerPolicies != null) {
+      headerPolicies.forEach(
+          p -> {
+            String x = (String) p;
+            List<String> pblcs = new LinkedList<>(Arrays.asList(x.split("[ ,]")));
+            pblcs.removeIf(n -> !n.contains(PUBLICATION_PREFIX));
+            policies.addAll(
+                pblcs.stream()
+                    .map(var -> var.replaceFirst(PUBLICATION_PREFIX, ""))
+                    .collect(Collectors.toList()));
+          });
+    }
+    return policies;
+  }
+
+  private boolean isEligibleForPublicationPolicyCheck(final MutableResponse response) {
+    return !isNotOKResponse(response) && !isNotJson(response);
+  }
+
+  private boolean isNotOKResponse(final MutableResponse response) {
+    return Response.Status.OK.getStatusCode() != response.getStatus();
+  }
+
+  private boolean isNotJson(final MutableResponse response) {
+    return !jsonConverter.isJson(response);
+  }
+
+  private Map<String, Object> extractContent(final MutableResponse response) {
+    return jsonConverter.readEntity(response);
+  }
+}
---
+++ api-policy-component-service/src/test/java/com/ft/up/apipolicy/filters/CheckPublicationPolicyTest.java
@@ -0,0 +1,96 @@
+package com.ft.up.apipolicy.filters;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.ft.up.apipolicy.JsonConverter;
+import com.ft.up.apipolicy.pipeline.HttpPipelineChain;
+import com.ft.up.apipolicy.pipeline.MutableRequest;
+import com.ft.up.apipolicy.pipeline.MutableResponse;
+import java.util.Collections;
+import javax.ws.rs.core.MultivaluedHashMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CheckPublicationPolicyTest {
+
+  @Mock private HttpPipelineChain mockChain;
+
+  private CheckPublicationPolicy checkPublicationPolicy =
+      new CheckPublicationPolicy(JsonConverter.testConverter());
+
+  private MutableRequest exampleRequest =
+      new MutableRequest(Collections.singleton("TEST"), getClass().getSimpleName());
+
+  @Test
+  public void ShouldReturnOK() {
+    String entity =
+        "{ \"type\":\"http://www.ft.com/ontology/content/Article\", \"bodyXML\": \"<body>something here</body>\",\"publication\":[\"8e6c705e-1132-42a2-8db0-c295e29e8658\",\"88fdde6c-2aa4-4f78-af02-9f680097cfd6\"] }";
+    MutableResponse validResponse =
+        new MutableResponse(new MultivaluedHashMap<>(), entity.getBytes());
+    validResponse.setStatus(200);
+    validResponse.getHeaders().putSingle("Content-Type", "application/json");
+    validResponse
+        .getHeaders()
+        .putSingle("X-Policy", "INTERNAL_UNSTABLE,PBLC_READ_8e6c705e-1132-42a2-8db0-c295e29e8658");
+    when(mockChain.callNextFilter(exampleRequest)).thenReturn(validResponse);
+
+    MutableResponse response = checkPublicationPolicy.processRequest(exampleRequest, mockChain);
+
+    assertThat(response.getStatus(), is(200));
+  }
+
+  @Test
+  public void ShouldReturn403() {
+    String entity =
+        "{ \"type\":\"http://www.ft.com/ontology/content/Article\", \"bodyXML\": \"<body>something here</body>\",\"publication\":[\"88fdde6c-2aa4-4f78-af02-9f680097cfd6\"] }";
+    MutableResponse validResponse =
+        new MutableResponse(new MultivaluedHashMap<>(), entity.getBytes());
+    validResponse.setStatus(200);
+    validResponse.getHeaders().putSingle("Content-Type", "application/json");
+    validResponse
+        .getHeaders()
+        .putSingle("X-Policy", "INTERNAL_UNSTABLE,PBLC_READ_8e6c705e-1132-42a2-8db0-c295e29e8658");
+    when(mockChain.callNextFilter(exampleRequest)).thenReturn(validResponse);
+
+    MutableResponse response = checkPublicationPolicy.processRequest(exampleRequest, mockChain);
+
+    assertThat(response.getStatus(), is(403));
+  }
+
+  @Test
+  public void ShouldReturnOkWithNoPublicationReadPolicyFtPink() {
+    String entity =
+        "{ \"type\":\"http://www.ft.com/ontology/content/Article\", \"bodyXML\": \"<body>something here</body>\",\"publication\":[\"88fdde6c-2aa4-4f78-af02-9f680097cfd6\"] }";
+    MutableResponse validResponse =
+        new MutableResponse(new MultivaluedHashMap<>(), entity.getBytes());
+    validResponse.setStatus(200);
+    validResponse.getHeaders().putSingle("Content-Type", "application/json");
+    validResponse.getHeaders().putSingle("X-Policy", "INTERNAL_UNSTABLE");
+    when(mockChain.callNextFilter(exampleRequest)).thenReturn(validResponse);
+
+    MutableResponse response = checkPublicationPolicy.processRequest(exampleRequest, mockChain);
+
+    assertThat(response.getStatus(), is(200));
+  }
+
+  @Test
+  public void ShouldDeniedAccessNoPublicationPolicyNonPinkFt() {
+    String entity =
+        "{ \"type\":\"http://www.ft.com/ontology/content/Article\", \"bodyXML\": \"<body>something here</body>\",\"publication\":[\"8e6c705e-1132-42a2-8db0-c295e29e8658\"] }";
+    MutableResponse validResponse =
+        new MutableResponse(new MultivaluedHashMap<>(), entity.getBytes());
+    validResponse.setStatus(200);
+    validResponse.getHeaders().putSingle("Content-Type", "application/json");
+    validResponse.getHeaders().putSingle("X-Policy", "INTERNAL_UNSTABLE");
+    when(mockChain.callNextFilter(exampleRequest)).thenReturn(validResponse);
+
+    MutableResponse response = checkPublicationPolicy.processRequest(exampleRequest, mockChain);
+
+    assertThat(response.getStatus(), is(403));
+  }
+}
